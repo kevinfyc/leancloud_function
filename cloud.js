6f56e4746b6060bc0c4a080965a01e2d567a3653
kevin <kevin@123.com>
2019-01-23 12:18:15 +0800

    first commit

--- cloud.js
+++ cloud.js
@@ -1,8 +1,38 @@
 var AV = require('leanengine');
+var https = require('https');
 
 /**
  * 一个简单的云代码方法
  */
 AV.Cloud.define('hello', function(request) {
   return 'Hello world!';
+});
+
+AV.Cloud.define('bdCode2session', function(request){
+	let code = request.code;
+	let client_id = request.client_id;
+	let sk = request.sk;
+
+	let url = 'https://openapi.baidu.com/nalogin/getSessionKeyByCode';
+	let req = https.request(url, (error, reponse, body) =>{
+		console.log(`error is ${error}`);
+		console.log(`reponse is ${reponse}`);
+		console.log(`body is ${body}`);
+		return [error, reponse, body];
+	});
+
+	req.on('error', (e)=>{
+	   console.log(`https error ${e.message}`);
+	   return [error, reponse, body];
+	});
+
+	let content = JSON.stringify({
+		code:code,
+		client_id:client_id,
+		sk:sk
+	});
+	console.log(`content is ${content}`);
+	req.write(content);
+
+	
 });
